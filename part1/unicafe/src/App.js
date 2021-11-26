0d58cbc3a6c451b7fabe1ea637792ffb2d09e937
Cartier Vu <vu.cartier@gmail.com>
2021-11-26 04:39:20 -0800

    Completed part1: unicafe

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+
+const Header = ({ text }) => <h2>{text}</h2>
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({ good, neutral, bad}) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return "No feedback given";
+  } else {
+    return (
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={good + neutral + bad} />
+          <StatisticLine text="average" value={getAverage(good, neutral, bad)} />
+          <StatisticLine text="positive" value={getPositivePercentage(good, neutral, bad)} />
+        </tbody>
+      </table>
+    )
+  }
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Header text="give feedback" />
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <Header text="statistics" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+const getAverage = (good, neutral, bad) => (good - bad) / (good + neutral + bad)
+
+const getPositivePercentage = (good, neutral, bad) => good * 100 / (good + neutral + bad) + " %"
+
+export default App
