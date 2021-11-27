cfb2091babc760bdc5186386385fc510a4db0961
Cartier Vu <vu.cartier@gmail.com>
2021-11-26 19:28:05 -0800

    Completed 2.9: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,9 +4,13 @@ import DisplayContacts from './components/Contact'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState()
+  const [filter, setFilter] = useState(null);
 
   const addPersons = (event) => {
     event.preventDefault();
@@ -33,16 +37,28 @@ const App = () => {
     setNewNumber(event.target.value);
   }
 
+  const handleFilterInputChange = (event) => {
+    setFilter(event.target.value);
+  }
+
+  const getFilteredPeople = () => {
+    return persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()));
+  }
+
+  const personsToShow = (filter == null) ? persons : getFilteredPeople()
+
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>filter shown with <input value={filter} onChange={handleFilterInputChange}/></div>
+      <h2>add a new</h2>
       <form onSubmit={addPersons}>
-        <div>name: <input value={newName}onChange={handleNameInputChange} /></div>
-        <div>number: <input value={newNumber}onChange={handleNumberInputChange} /></div>
+        <div>name: <input value={newName} onChange={handleNameInputChange} /></div>
+        <div>number: <input value={newNumber} onChange={handleNumberInputChange} /></div>
         <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-      <DisplayContacts contacts={persons}/>
+      <DisplayContacts contacts={personsToShow}/>
     </div>
   )
 }
